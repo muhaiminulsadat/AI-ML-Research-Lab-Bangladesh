93dfc8cee9f2fd48c4da603e259ea71dc89001ac
Sadat <muhaiminulsadat@gmail.com>
2026-04-07 17:27:07 +0600

    hide TeamSection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,7 @@ export default function HomePage() {
         <MissionSection />
         <DomainsSection />
         <ProjectsShowcase />
-        <TeamSection />
+        {/* <TeamSection /> */}
         <DepartmentsSection />
         <CTASection />
       </main>
