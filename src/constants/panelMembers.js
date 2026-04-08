7dbbbddcc67496eb8785f82ccf12811b4f436c5b
Sadat <muhaiminulsadat@gmail.com>
2026-04-08 15:15:58 +0600

    make panel member data

---
+++ src/constants/panelMembers.js
@@ -0,0 +1,80 @@
+const memberData = [
+  {
+    "name": "Arafat Yeasmin Oyshi",
+    "url": "https://drive.google.com/thumbnail?id=1Q4I8_ymZOGW9-FYMptVXXR5TOrraHBwm&sz=w800"
+  },
+  {
+    "name": "Farhan Labib Ahan",
+    "url": "https://drive.google.com/thumbnail?id=1oMdJnLpMymvUTn7GiiKxGt-Ie5oWrlxn&sz=w800"
+  },
+  {
+    "name": "Mahi Tasnim",
+    "url": "https://drive.google.com/thumbnail?id=1QI0Lu5ggkRzZokaKZdwvJQoxheIQQmST&sz=w800"
+  },
+  {
+    "name": "Rajoy",
+    "url": "https://drive.google.com/thumbnail?id=1Qs7PYbd9UxG_rgIo2-uCWjw_YLMqtS72&sz=w800"
+  },
+  {
+    "name": "Samyo Pramanik",
+    "url": "https://drive.google.com/thumbnail?id=1N1bmCMhPLfJd-zTxt1iz2gRYNjFifKTn&sz=w800"
+  },
+  {
+    "name": "Mazedul Hoque",
+    "url": "https://drive.google.com/thumbnail?id=1OXH827xYgQgGKAycqkYaQiRFVju4bOZc&sz=w800"
+  },
+  {
+    "name": "Shourob Biswas",
+    "url": "https://drive.google.com/thumbnail?id=1xO_oINxM5zazIlCXerqzm_-AutCn41G_&sz=w800"
+  },
+  {
+    "name": "Md. Taufiq Biswas",
+    "url": "https://drive.google.com/thumbnail?id=1MTGpKMsZrfBuvWo0UEeiv8Qxne8hOuP2&sz=w800"
+  },
+  {
+    "name": "Himel Sarker Turjo",
+    "url": "https://drive.google.com/thumbnail?id=16Vd7N7fbMkl9vrx3VhOTMRdZ_Z96OavB&sz=w800"
+  },
+  {
+    "name": "Anik Kumar Ray",
+    "url": "https://drive.google.com/thumbnail?id=1NDqDI4snIHzjpMVsptU34hnZ_M7nIHo5&sz=w800"
+  },
+  {
+    "name": "Mithun Kumar Roy",
+    "url": "https://drive.google.com/thumbnail?id=1ipCL7vS-oNLLBHnA7hJHbGpCyngcSU6p&sz=w800"
+  },
+  {
+    "name": "Prachya Biswas",
+    "url": "https://drive.google.com/thumbnail?id=1cdrzhe-8m_es9_jB9FS6PkuIxGOg2yha&sz=w800"
+  },
+  {
+    "name": "Prachya Biswas",
+    "url": "https://drive.google.com/thumbnail?id=1sdlOVsQ--_WTXmGAB25XQxLNyCE37qUy&sz=w800"
+  },
+  {
+    "name": "Md.Tafim Moula",
+    "url": "https://drive.google.com/thumbnail?id=103bb4u8F6BpCwTSqxDThlfotNvtxHmTH&sz=w800"
+  },
+  {
+    "name": "Atiqur Rahman Atik",
+    "url": "https://drive.google.com/thumbnail?id=1dPOFLrQ08gNCnjvcTo9VNknCuHraNqJ5&sz=w800"
+  },
+  {
+    "name": "Torun Kumar Saha Tirtho",
+    "url": "https://drive.google.com/thumbnail?id=1go9sKblPeMqPaS_wwmGqpwsSJTXrhWBR&sz=w800"
+  },
+  {
+    "name": "Nirob Devnath",
+    "url": "https://drive.google.com/thumbnail?id=1BQtAhYb1jaZg594TVgPbMU0LcTdB4hDw&sz=w800"
+  },
+  {
+    "name": "Rudro Mohonto Jit",
+    "url": "https://drive.google.com/thumbnail?id=1FDNFbeYxpzHixmavrZJy82HtuvsF9Nt3&sz=w800"
+  },
+  {
+    "name": "Md. Ashfik Sadman",
+    "url": "https://drive.google.com/thumbnail?id=1ZDS_l4wOd_6Yh6vkvdNDd40RStJ3bJ33&sz=w800"
+  }
+];
+
+export default memberData;
